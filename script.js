db03e9213be91700444bf73f2eb538b5f5857d5f
Denis Antonov <antonov25041992@yandex.ru>
2020-10-28 16:42:05 +0300

    fix: edit after code-review

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let buttonEditProfile = page.querySelector('.button_type_edit-profile');
 let formElement = page.querySelector('.popup__form');
 
 let popupEditProfile = document.querySelector('.popup');
+let buttonClosePopupEditProfile = popupEditProfile.querySelector('.button_type_close-popup');
 let popupEditProfileNameInput = popupEditProfile.querySelector('.popup__item_el_name');
 let popupEditProfileJobInput = popupEditProfile.querySelector('.popup__item_el_job');
 
 function closePopupEditProfileHandler() {
   popupEditProfile.classList.remove('popup_opened');
-  page.classList.remove('page_overflow_hidden');
 }
 
 function formSubmitHandler(event) {
@@ -21,14 +21,11 @@ function formSubmitHandler(event) {
 }
 
 function editProfileHandler() {
-  let buttonClosePopupEditProfile = popupEditProfile.querySelector('.button_type_close-popup');
-  buttonClosePopupEditProfile.addEventListener('click', closePopupEditProfileHandler);
-
   popupEditProfile.classList.add('popup_opened');
-  page.classList.add('page_overflow_hidden');
   popupEditProfileNameInput.value = profileName.textContent;
   popupEditProfileJobInput.value = profileJob.textContent;
 }
 
 buttonEditProfile.addEventListener('click', editProfileHandler);
+buttonClosePopupEditProfile.addEventListener('click', closePopupEditProfileHandler);
 formElement.addEventListener('submit', formSubmitHandler)
