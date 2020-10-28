2dfa15c756f39fc4a587fee72430771a6c6dc021
Denis Antonov <antonov25041992@yandex.ru>
2020-10-28 11:42:02 +0300

    feat: add profile editing feature

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 let page = document.querySelector('.page');
+let profileName = page.querySelector('.profile__name');
+let profileJob = page.querySelector('.profile__job');
+let buttonEditProfile = page.querySelector('.button_type_edit-profile');
+let formElement = page.querySelector('.popup__form');
+
 let popupEditProfile = document.querySelector('.popup');
 let popupEditProfileNameInput = popupEditProfile.querySelector('.popup__item_el_name');
-let popupEditProfileJobInput = document.querySelector('.popup__item_el_job');
-let buttonEditProfile = document.querySelector('.button_type_edit-profile');
-let buttonClosePopupEditProfile = document.querySelector('.button_type_close-popup');
+let popupEditProfileJobInput = popupEditProfile.querySelector('.popup__item_el_job');
 
-function buttonEditProfileHandler() {
-  popupEditProfile.classList.add('popup_opened');
-  page.classList.add('page_overflow_hidden');
-}
-
-function buttonClosePopupEditProfileHandler() {
+function closePopupEditProfileHandler() {
   popupEditProfile.classList.remove('popup_opened');
   page.classList.remove('page_overflow_hidden');
 }
 
-buttonEditProfile.addEventListener('click', buttonEditProfileHandler);
-buttonClosePopupEditProfile.addEventListener('click', buttonClosePopupEditProfileHandler);
+function formSubmitHandler(event) {
+  event.preventDefault();
+  profileName.textContent = popupEditProfileNameInput.value;
+  profileJob.textContent = popupEditProfileJobInput.value;
+  closePopupEditProfileHandler();
+}
+
+function editProfileHandler() {
+  let buttonClosePopupEditProfile = popupEditProfile.querySelector('.button_type_close-popup');
+  buttonClosePopupEditProfile.addEventListener('click', closePopupEditProfileHandler);
+
+  popupEditProfile.classList.add('popup_opened');
+  page.classList.add('page_overflow_hidden');
+  popupEditProfileNameInput.value = profileName.textContent;
+  popupEditProfileJobInput.value = profileJob.textContent;
+}
+
+buttonEditProfile.addEventListener('click', editProfileHandler);
+formElement.addEventListener('submit', formSubmitHandler)
